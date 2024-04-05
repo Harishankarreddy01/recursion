8e19c198ff56849baaf58229a3f7f260c5525f92
Harishankarreddy01 <harishankarreddy02@gmail.com>
2024-04-05 16:32:59 -0400

    Sum of Digits in a given number

--- sumOfDigits.java
+++ sumOfDigits.java
@@ -1,7 +1,7 @@
 public class sumOfDigits {
     int Sum(int n){
-        if(n<10){
-            return n;
+        if(n==0){
+            return 0;
         }
         return(n%10 + Sum(n/10));
     }
